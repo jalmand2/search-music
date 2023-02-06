c3f9ec2c6a3c057b19db33c203c9ac74bf110734
Jessica Almand <jlalmand27@gmail.com>
2023-02-05 19:23:22 -0500

    Starting to get navbar figured out, commented out container class, looks better

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,90 @@
-// import logo from './logo.svg';
-import React, {useState} from 'react';
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import React, {useState} from 'react';
 import './App.css';
+import Auth from './utils/auth';
 import Nav from './components/Nav';
 import Home from './components/Home';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import ProtectRoute from './components/ProtectRoute';
 
-function App() {
-  const [pageIndex, setPageIndex] = useState(0);
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
-  return (
+/// SET CONTEXT ///
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
-    <div id="canvas" class="root">
+/// SET UP CLIENT ///
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+// function App() {
+//   const [pageIndex, setPageIndex] = useState(0);
+
+//   return (
+
+//     <div id="canvas" class="root">
      
-      <Nav
-        showHome={() => setPageIndex(0)} />
+//       <Nav
+//         showHome={() => setPageIndex(0)} />
 
-      {pageIndex === 0 ? (
-        <Home />
-      ) : (
-        <Home />
-      )}
-      <footer className='text-center text-lg-left'>
-                <div className='text-center p-3'>
-                <a style={{color: 'black'}} href="https://github.com/jalmand2"><iconify-icon icon="ri:github-fill" width="100" height="100"></iconify-icon></a>
-                <a style={{color: 'black'}} href="https://www.linkedin.com/in/jessicaalmand/"><iconify-icon icon="mdi:linkedin" width="100" height="100"></iconify-icon></a>
-                <a style={{color: 'black'}} href="https://www.facebook.com/jessica.almand/"><iconify-icon icon="ic:baseline-facebook" width="100" height="100"></iconify-icon></a>
-                <p style={{color: 'black'}}>© Developed by Jessica Almand</p>
-                </div>
-              </footer>
+//       {pageIndex === 0 ? (
+//         <Home />
+//       ) : (
+//         <Home />
+//       )}
+//       <footer className='text-center text-lg-left'>
+//                 <div className='text-center p-3'>
+//                 <a style={{color: 'black'}} href="https://github.com/jalmand2"><iconify-icon icon="ri:github-fill" width="100" height="100"></iconify-icon></a>
+//                 <a style={{color: 'black'}} href="https://www.linkedin.com/in/jessicaalmand/"><iconify-icon icon="mdi:linkedin" width="100" height="100"></iconify-icon></a>
+//                 <a style={{color: 'black'}} href="https://www.facebook.com/jessica.almand/"><iconify-icon icon="ic:baseline-facebook" width="100" height="100"></iconify-icon></a>
+//                 <p style={{color: 'black'}}>© Developed by Jessica Almand</p>
+//                 </div>
+//               </footer>
 
               
-    </div>
+//     </div>
                   
-);
+// );
+// }
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Nav />
+        <div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+          <Route
+            path='dashboard'
+            element={Auth.loggedIn() ? <Dashboard /> : <ProtectRoute />}
+          />
+        </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
 }
-
 export default App;
--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,45 +1,135 @@
-import React from 'react';
-
-
-const Nav = ({ showHome}) => {
-    return ( 
-       
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid" id="naviePoo">
-      {/* <!-- <a class="navbar-brand" href="#">Navbar</a> --> */}
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav" id="navPoo">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#ex">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#ex">Saved Songs</a>
-          </li>
-          {/* <!-- <li class="nav-item">
-            <a class="nav-link" href="#">Login</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled hidden">Logout</a>
-          </li> --> */}
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#ex" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu" id="dropBox">
-              <li><a class="dropdown-item hidden" href="#ex">My Account</a></li>
-              <li><a class="dropdown-item" href="#ex">FAQ</a></li>
-              <li><hr class="dropdown-divider"></hr></li>
-              <li><a class="dropdown-item" href="#ex">Sign in</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav> 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+// npm install react-bootstrap bootstrap
+const Navigation = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-  };
+}
 
-export default Nav;
+export default Navigation;
+// // import React from 'react';
+// import React, { useState } from 'react';
+
+
+// const Nav = () => {
+//   const [toggle, setToggle] = useState(true);
+//   return (
+//     <>
+//       <button 
+//             onClick={() => setToggle(!toggle)} 
+//             class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//           <span class="navbar-toggler-icon"></span>
+//       </button>
+//       {toggle && (
+//         // <ul class="list-group">
+//         //   <li class="list-group-item">An item</li>
+//         //   <li class="list-group-item">A second item</li>
+//         //   <li class="list-group-item">A third item</li>
+//         //   <li class="list-group-item">A fourth item</li>
+//         //   <li class="list-group-item">And a fifth one</li>
+//         // </ul>
+//             <nav class="navbar navbar-expand-lg bg-body-tertiary">
+//     <div class="container-fluid" id="naviePoo">
+//       {/* <!-- <a class="navbar-brand" href="#">Navbar</a> --> */}
+//       {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//         <span class="navbar-toggler-icon"></span>
+//       </button> */}
+//       <div class="collapse navbar-collapse" id="navbarNav">
+//         <ul class="navbar-nav" id="navPoo">
+//           <li class="nav-item">
+//             <a class="nav-link active" aria-current="page" href="#ex">Home</a>
+//           </li>
+//           <li class="nav-item">
+//             <a class="nav-link" href="#ex">Saved Songs</a>
+//           </li>
+//           {/* <!-- <li class="nav-item">
+//             <a class="nav-link" href="#">Login</a>
+//           </li>
+//           <li class="nav-item">
+//             <a class="nav-link disabled hidden">Logout</a>
+//           </li> --> */}
+//           <li class="nav-item dropdown">
+//             <a class="nav-link dropdown-toggle" href="#ex" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//               Dropdown
+//             </a>
+//             <ul class="dropdown-menu" id="dropBox">
+//               <li><a class="dropdown-item hidden" href="#ex">My Account</a></li>
+//               <li><a class="dropdown-item" href="#ex">FAQ</a></li>
+//               <li><hr class="dropdown-divider"></hr></li>
+//               <li><a class="dropdown-item" href="#ex">Sign in</a></li>
+//             </ul>
+//           </li>
+//         </ul>
+//       </div>
+//     </div>
+//   </nav> 
+//       )}
+//     </>
+//   )
+// };
+//   //   return ( 
+       
+//   //   <nav class="navbar navbar-expand-lg bg-body-tertiary">
+//   //   <div class="container-fluid" id="naviePoo">
+//   //     {/* <!-- <a class="navbar-brand" href="#">Navbar</a> --> */}
+//   //     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//   //       <span class="navbar-toggler-icon"></span>
+//   //     </button>
+//   //     <div class="collapse navbar-collapse" id="navbarNav">
+//   //       <ul class="navbar-nav" id="navPoo">
+//   //         <li class="nav-item">
+//   //           <a class="nav-link active" aria-current="page" href="#ex">Home</a>
+//   //         </li>
+//   //         <li class="nav-item">
+//   //           <a class="nav-link" href="#ex">Saved Songs</a>
+//   //         </li>
+//   //         {/* <!-- <li class="nav-item">
+//   //           <a class="nav-link" href="#">Login</a>
+//   //         </li>
+//   //         <li class="nav-item">
+//   //           <a class="nav-link disabled hidden">Logout</a>
+//   //         </li> --> */}
+//   //         <li class="nav-item dropdown">
+//   //           <a class="nav-link dropdown-toggle" href="#ex" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//   //             Dropdown
+//   //           </a>
+//   //           <ul class="dropdown-menu" id="dropBox">
+//   //             <li><a class="dropdown-item hidden" href="#ex">My Account</a></li>
+//   //             <li><a class="dropdown-item" href="#ex">FAQ</a></li>
+//   //             <li><hr class="dropdown-divider"></hr></li>
+//   //             <li><a class="dropdown-item" href="#ex">Sign in</a></li>
+//   //           </ul>
+//   //         </li>
+//   //       </ul>
+//   //     </div>
+//   //   </div>
+//   // </nav> 
+//   // );
+//   // };
+
+// export default Nav;
